85ca41300170b7eec741854e4f17e7b6af3b0c9f
csilla bukki <csillabukki@csillas-MacBook-Air.local>
2017-09-27 10:59:36 -0700

    5.3.5

--- natur_server/models/questions.js
+++ natur_server/models/questions.js
@@ -4,9 +4,10 @@ var answerSchema = new mongoose.Schema({
   answer: {type: String, required: true}
 });
 
-var qustionSchema = new mongoose.Schema({
+var questionSchema = new mongoose.Schema({
     number: {type: Number, min: 1, max: 100},
     question: { type: String, required: true},
     answers: [answerSchema]
 });
 
+mongoose.model('QuestionAnswer', questionSchema);
