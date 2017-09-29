225056803d3c1af7b201b24757aba3a618e644b3
csilla bukki <csillabukki@csillas-MacBook-Air.local>
2017-09-29 11:25:32 -0700

    5.4

--- natur_server/models/questions.js
+++ natur_server/models/questions.js
@@ -10,4 +10,63 @@ var questionSchema = new mongoose.Schema({
     answers: [answerSchema]
 });
 
-mongoose.model('QuestionAnswer', questionSchema);
+mongoose.model('Questionanswer', questionSchema);
+
+//put in consol mongo shell 
+/*
+db.questionanswers.save({
+    number: 1,
+    question: 'What is the supreme law of the land?'
+})
+
+db.questionanswers.update({
+    number: 1
+}, {
+    $push: {
+        answers: {
+        answer: '* the Constitution'
+        }
+    }
+})
+
+**********************************************************
+
+db.questionanswers.save({
+    number: 2,
+    question: 'What does the Constitution do?'
+})
+
+db.questionanswers.update({
+    number: 2
+}, {
+    $push: {
+        answers: {
+            $each: [{
+                asnwer: '* sets up the government'
+            },
+               {
+               answer: '* defines the government'
+               },
+               {
+               answer: '* protects basic rights of Americans'
+               }
+            ]
+        }
+    }
+})
+db.questionanswers.save({
+    number: 3,
+    question: 'The idea of self-government is in the first three words of the Constitution. What are these words?'
+})
+
+db.questionanswers.update({
+    number: 3
+}, {
+    $push: {
+        answers: {
+        answer: '* We the People'
+        }
+    }
+})
+
+*/
